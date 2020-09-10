ffd7ef9299d700ae60e6fcb98d56d45aa7e0c5d7
bvandyke1 <47338862+bvandyke1@users.noreply.github.com>
2020-09-10 10:18:52 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/* User Data 01.cpp
+*  Brandon VanDyke
+*  Date last updated: 02/04/2019
+* Purpose: Display student data
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string CourseName = "Computer Science I"; //string variable for course name
+    string AssignName = "Module 3 Lab 1"; //string variable for assignment name
+    string AuthorName = "Brandon VanDyke"; //string for author of program
+    string DueDate = "02/04/19"; //string for due date
+    
+    cout << CourseName << endl; //display course name
+    cout << AssignName << endl; //display assignment
+    cout << AuthorName << endl; //display author name
+    cout << DueDate << endl; //display due date
+    
+    cout << "III" << endl; //first line of "I can program"
+    cout << " I     ccc  aa nn   ppp rrr ooo ggg rrr  aa mmm" << endl; //display seecond line
+    cout << " I     c   a a n n  p p r   o o g g r   a a mmm" << endl; //display third line
+    cout << " I     ccc aaa n n  ppp r   ooo  gg r   aaa m m" << endl; //display fourth line
+    cout << "III                 p           ggg            " << endl; //display final line 
+    
+
+    return 0;
+}
+
